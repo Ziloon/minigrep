e25e75574946181da3b3b12f42a5094b2b4f288d
Zhao Ziloong <ziloon.z@icloud.com>
2024-04-09 10:07:13 +0800

    所有实现在main里

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let quera = &args[1];
+    let filename = &args[2];
+    println!("{}", quera);
+    println!("{}", filename);
+
+    let context = fs::read_to_string(filename).unwrap();
+    println!("\n-------------[{}] context-------------", filename);
+    println!("{}", context);
 }
