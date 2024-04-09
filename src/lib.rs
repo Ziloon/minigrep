9dad5ffab6e8a95d5fddd4587ed15e588223e1c0
Zhao Ziloong <ziloon.z@icloud.com>
2024-04-09 10:22:25 +0800

    独立出来cargo

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: Vec<String>) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
+}
