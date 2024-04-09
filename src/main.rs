b51a617e639dbd390d640461973b72f96ebb89dc
Zhao Ziloong <ziloon.z@icloud.com>
2024-04-09 10:22:02 +0800

    创建一个 Config 的构造函数

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use std::{env, fs};
-
+use minigrep;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let quera = &args[1];
-    let filename = &args[2];
-    println!("{}", quera);
-    println!("{}", filename);
+    let config = minigrep::Config::new(env::args().collect());
+    println!("{:?}", config);
 
-    let context = fs::read_to_string(filename).unwrap();
-    println!("\n-------------[{}] context-------------", filename);
+    let context = fs::read_to_string(&config.filename).unwrap();
+    println!("\n-------------[{}] context-------------", config.filename);
     println!("{}", context);
 }
